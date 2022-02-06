bfaf9bea87eca5e979f2aaceed889c481bb22d28
Netanel <natanelmich103@gmail.com>
2022-02-06 16:28:27 +0200

    Added Basic Navbar, Sign-In & Sign-Up Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+// Pages:
+import Navbar from './components/Navbar'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route 
+          path="/signin"
+          element={<Login />}
+        />
+        <Route 
+          path="/signup"
+          element={<Register />}
+        />
+      </Routes>
     </div>
   );
 }
